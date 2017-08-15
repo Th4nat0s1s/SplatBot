f00b401ba1861cf11ffc57c3940758bff8b643b9
Th4nat0s1s <sllimbaugh@gmail.com>
2017-08-15 00:29:14 -0400

    Update SplatBot.py

--- SplatBot.py
+++ SplatBot.py
@@ -1,20 +1,31 @@
 import discord
 from discord.ext import commands
-import asyncio
 
 description = '''A fresh Bot for all of your Splatoon needs!'''
 
-bot = commands.Bot(command_prefix='?', description=description)
+bot = commands.Bot(command_prefix='//', description=description)
 
 @bot.event
-async def on_ready(pass_context=True):
+async def on_ready():
    print("SplatBot Online!")
    print("Name: {}".format(bot.user.name))
    print("ID: {}".format(bot.user.id))
    print("-----")
 
-@bot.command()
-async def ping(ctx):
-   await bot.say("Pong!".format(ctx))
+@bot.command
+async def ping():
+    await bot.say("Pong!")
+
+@bot.command(help="Use //reg to view the Regular Battle schedule for the next four(4) hours!")
+async def reg():
+    await bot.say("")
+
+@bot.command
+async def rank():
+    await bot.say("")
+
+@bot.command
+async def leag():
+    await bot.say("")
 
 bot.run="redacted"
